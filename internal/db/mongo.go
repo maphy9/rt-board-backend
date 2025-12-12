49c871bfccbc554084908088f5c48ee213eefbe5
maphy9 <maphy9@gmail.com>
2025-12-12 15:52:56 +0100

    feat: Add setup functions for MongoDB

---
+++ internal/db/mongo.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func Connect(uri string) (*mongo.Client, error) {
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, fmt.Errorf("invalid MongoDB URI: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, fmt.Errorf("MongoDB ping failed: %w", err)
+	}
+
+	return client, nil
+}
+
+func Disconnect(client *mongo.Client) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+
+	return client.Disconnect(ctx)
+}
